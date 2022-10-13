b830ccc1e1e6653acea8d03857a7ea791f762093
Mike R <michaelangelram8@gmail.com>
2022-10-13 10:59:15 -0500

    Insert Stopwatch component into App.js

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import Control from "./components/Control";
-import { displayTime } from "./components/utils";
+import { StyleSheet, View } from "react-native";
+import Stopwatch from "./components/Stopwatch";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Control />
+      <Stopwatch />
     </View>
   );
 }
@@ -16,8 +12,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
